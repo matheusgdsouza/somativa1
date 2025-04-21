c878d6e79ba2db2312a1ea4901476ba32b58afdc
matheusgdsouza <matheusgds3011@gmail.com>
2025-04-21 02:48:53 -0300

    Adicionar funcionalidade de verificação de idade

--- main.py
+++ main.py
@@ -2,6 +2,19 @@ def saudacao(nome):
     """Esta função cumprimenta a pessoa passada como parâmetro."""
     print(f"Olá, {nome}!")
 
-    if __name__ == "__main__":
-        nome_usuario = input("Digite seu nome: ")
-        saudacao(nome_usuario)
+def verificar_idade(idade):
+    """Verifica a idade e imprime uma mensagem."""
+    if idade < 18:
+        print("Você é menor de idade.")
+    else:
+        print("Você é maior de idade.")
+
+if __name__ == "__main__":
+    nome_usuario = input("Digite seu nome: ")
+    saudacao(nome_usuario)
+
+    try:
+        idade_usuario = int(input("Digite sua idade: "))
+        verificar_idade(idade_usuario)
+    except ValueError:
+        print("Por favor, digite um número inteiro para a idade.")
