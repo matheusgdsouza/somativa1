7798b441abcb45eeb78a3515096b9a53de87f222
matheusgdsouza <matheusgds3011@gmail.com>
2025-04-27 20:50:58 -0300

    Implementação docker

--- main.py
+++ main.py
@@ -1,18 +1,36 @@
+from fastapi import FastAPI
+
 def saudacao(nome):
-    """Esta função cumprimenta a pessoa passada como parâmetro."""
+    """Este função cumprimenta a pessoa passada como parâmetro"""
     print(f"Olá, {nome}! Seja bem-vindo(a)!")
+    return {"mensagem": f"Olá, {nome}! Seja bem-vindo(a)!"}
 
 def verificar_idade(idade):
-    """Verifica a idade e imprime uma mensagem."""
+    """Verifica a idade e imprime uma mensagem"""
     if idade < 18:
         print("Você é menor de idade.")
+        return {"mensagem": "Você é menor de idade."}
     else:
         print("Você é maior de idade.")
+        return {"mensagem": "Você é maior de idade."}
+
+app = FastAPI()
+
+@app.get("/saudacao/{nome}")
+async def rota_saudacao(nome: str):
+    return saudacao(nome)
+
+@app.get("/verificar_idade/{idade}")
+async def rota_verificar_idade(idade: int):
+    return verificar_idade(idade)
+
+@app.get("/")
+async def root():
+    return {"message": "API com suas funções!"}
 
 if __name__ == "__main__":
     nome_usuario = input("Digite seu nome: ")
     saudacao(nome_usuario)
-
     try:
         idade_usuario = int(input("Digite sua idade: "))
         verificar_idade(idade_usuario)
